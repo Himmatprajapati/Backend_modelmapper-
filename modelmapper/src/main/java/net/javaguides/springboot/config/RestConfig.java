7244c60aaa2ad02efd8bb6459eeeb415a5ff0ade
HimmatPrajapati <123347334+Himmatprajapati@users.noreply.github.com>
2023-03-14 19:37:45 +0530

    Add files via upload

---
+++ modelmapper/src/main/java/net/javaguides/springboot/config/RestConfig.java
@@ -0,0 +1,20 @@
+package net.javaguides.springboot.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestConfig {
+
+       @Bean
+	public ModelMapper modelMapper() {
+		return  new ModelMapper();
+		
+	}
+	
+
+	
+	
+}
---
+++ modelmapper/src/main/java/net/javaguides/springboot/dto/EmployeeDto.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot.dto;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class EmployeeDto {
+	
+	private String firstName;
+	//private String lastName;
+	
+}
